03263fec449af11dbe7fb2dbbde4d1a515bed5e7
Jamirul islam <rowjoy202@gmail.com>
2022-10-21 22:47:40 +0600

    New add

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static Color blackColor = const Color(0x003d3d6b);
+  static Color whiteColor = const Color(0x00ffffff);
+  static Color themeColor = const Color(0x00ff6b35);
+  static Color subThemeColor = const Color(0x00004e89);
+  static Color backGroundColor = const Color(0x00f7c59f).withOpacity(0.5);
+}
