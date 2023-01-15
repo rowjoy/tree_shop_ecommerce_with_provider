91b980bd55ee20c8c225d5c6278c0c3d4ac0ee17
Jamirul islam <rowjoy202@gmail.com>
2023-01-16 00:32:53 +0600

    Slider design

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,10 +1,23 @@
 // ignore_for_file: non_constant_identifier_names
 
 
+import 'package:flutter/material.dart';
+
 class AppColors {
-  static String greenColor = "#02231C";
-  static String seGreenColor = "#91F086";
-  static String midelGreenColor = "#48BF53";
+  static String greenColor = "#06283D";
+  static String seGreenColor = "#A6BB8D";
+  static String midelGreenColor = "#A6BB8D";
   static String whiteColor = "#EEF4F4";
-  static String blackColor = "##000000";
+  static String blackColor = "#000000";
+  static String grayColors = "#DFF6FF";
+}
+
+class GradientColors{
+
+  static getGradient(Color color ,) {
+    return LinearGradient(
+      colors: [color.withOpacity(0.5), color.withOpacity(0.1)],
+      stops: const [0.0, 0.7]);
+  }
+
 }
--- lib/modules/views/prodact_screen.dart
+++ lib/modules/views/prodact_screen.dart
@@ -4,6 +4,7 @@
 
 
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -19,6 +20,7 @@ class ProdactScreen extends StatefulWidget {
 
 class _ProdactScreenState extends State<ProdactScreen> {
   List<String> categories = ["Rose", "Water Lily", "Beli flower" , "Sunflower" , "Marigold"];
+  int sliderindex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -100,7 +102,7 @@ class _ProdactScreenState extends State<ProdactScreen> {
                       height: 50,
                       width: 50,
                       decoration: BoxDecoration(
-                        color: HexColor(AppColors.greenColor),
+                        gradient: GradientColors.getGradient(HexColor(AppColors.greenColor)),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Icon(Icons.bar_chart_rounded, color: HexColor(AppColors.whiteColor,),size: 35,),
@@ -109,7 +111,105 @@ class _ProdactScreenState extends State<ProdactScreen> {
                 ),
               ),
               SizedBox(height: 15,),
-              
+              Container(
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                child: PageView.builder(
+                  itemBuilder: (
+                    BuildContext context , index){
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: GradientColors.getGradient(HexColor(AppColors.greenColor))
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Upcomming Tree...",
+                                      style: TextStyle(
+                                        color: HexColor(AppColors.greenColor),
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text("Rose",
+                                      style: TextStyle(
+                                        color: HexColor(AppColors.greenColor),
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text.rich(
+                                      TextSpan(
+                                        text: "Up to ".toUpperCase(),
+                                        style: TextStyle(
+                                          color: HexColor(AppColors.greenColor),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text: "70%".toUpperCase(),
+                                            style: TextStyle(
+                                              color: Colors.red,
+                                              fontSize: 30,
+                                              fontWeight: FontWeight.bold,
+                                            )
+                                          ),
+                                          TextSpan(
+                                            text: " Off".toUpperCase(),
+                                            style: TextStyle(
+                                              color: HexColor(AppColors.blackColor),
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                        ]
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ) ,
+                            ),
+                            Expanded(
+                              child: Image.asset("assets/images/onboard_tree.png"),
+                            ),
+                          ],
+                        ),
+                      );
+                  },
+                  itemCount:10,
+                  reverse: true,
+                  onPageChanged: (value){
+                    setState(() {
+                      sliderindex = value;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(height: 8,),
+              Container(
+                height: 20,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                   itemBuilder: (BuildContext context, index){
+                       return Padding(
+                         padding: const EdgeInsets.all(4.0),
+                         child: CircleAvatar(
+                           radius: 5,
+                           backgroundColor: sliderindex == index ? HexColor(AppColors.greenColor) : Colors.grey,
+                          ),
+                       );
+                   },
+                 ),
+              ),
             ],
           ),
         ),
